4d7e85899db58f1075351ec5cdf6b9082e9de9a0
Lukas Hermann <hey@lukashermann.dev>
2024-07-18 17:51:08 +0200

    feat: add DocumentSnapshot and UserRecord

--- src/FirebaseBuiltins.ts
+++ src/FirebaseBuiltins.ts
@@ -1,6 +1,17 @@
+//
+// Firestore
+//
+
 export type DocumentSnapshotId = string
 
-export type UserRecordId = string
+export type DocumentSnapshot = {
+  id: DocumentSnapshotId
+  [key: string]: any
+  createTime?: FirestoreTimestamp | undefined
+  updateTime?: FirestoreTimestamp | undefined
+  exists?: boolean
+  ref?: any
+}
 
 export type FirestoreTimestamp = {
   nanoseconds: number
@@ -8,4 +19,39 @@ export type FirestoreTimestamp = {
   toDate: () => Date
   toMillis: () => number
   toString: () => string
+}
+
+//
+// Auth
+//
+
+export type UserRecordId = string
+
+export type UserRecord = {
+  uid: UserRecordId
+  email?: string | null
+  emailVerified?: boolean
+  displayName?: string | null
+  photoURL?: string | null
+  phoneNumber?: string | null
+  disabled?: boolean
+  metadata?: {
+    creationTime?: string
+    lastSignInTime?: string
+  }
+  providerData?: Array<{
+    uid: string
+    displayName?: string | null
+    email?: string | null
+    phoneNumber?: string | null
+    photoURL?: string | null
+    providerId: string
+  }>
+  passwordHash?: string
+  passwordSalt?: string
+  customClaims?: {
+    [key: string]: any
+  }
+  tokensValidAfterTime?: string
+  tenantId?: string | null
 }
